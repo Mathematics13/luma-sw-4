02d74050e5b530777c754f8f8f69d84fcb19622c
Mathematics13 <haripriyakrishnan4@gmail.com>
2023-12-03 20:47:30 +0000

    Week 13

---
+++ src/test/java/com/magento/softwaretestingboard/testsuite/MenTest.java
@@ -0,0 +1,29 @@
+package com.magento.softwaretestingboard.testsuite;
+
+import com.magento.softwaretestingboard.pages.MenPage;
+import com.magento.softwaretestingboard.testbase.TestBase;
+import org.testng.annotations.Test;
+
+public class MenTest extends TestBase
+{
+    MenPage menPage = new MenPage();
+    @Test
+    public void userShouldAddProductSuccessfullyToshoppingCart()
+    {
+        menPage.mouseHoverOnMenMenu();
+        menPage.mouseHoverOnBottoms();
+        menPage.mouseHoverOnPantsAndClick();
+        menPage.mouseHoverToCronusYogaPant();
+        menPage.mouseHoverToCronusYogaPantAndClickSize32();
+        menPage.mouseHoverToCronusYogaPantAndClickBlackColour();
+        menPage.mouseHoverToCronusAndClickOnAddToCartButton();
+        menPage.mouseHoverToCronusYogaPant1();
+        menPage.mouseHoverToCronusAndClickOnAddToCartButton1();
+        menPage.getTextFromAddedCronusPantInYourShoppingCart();
+        menPage.clickOnShoppingCartLinkIntoMessage();
+        menPage.verifyShoppingCartText();
+        menPage.verifyCronusYogaPantText();
+        menPage. verifyProductsize32Text();
+        menPage.verifyBlackColourText();
+    }
+}
